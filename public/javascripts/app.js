95a179dfd6350050e0458268efd59e4f3c02aa28
Kayla Bixler <kaylabix@comcast.net>
2017-12-01 12:39:28 -0700

    WIP: midterm

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -19,7 +19,6 @@ function MainCtrl($scope, $http) //controller
     // //variables
     $scope.products = [];
     $scope.cart = [];
-    $scope.picURL = picURL;
 
     function getAll()  //get Mongoose database, if successful copy the data into the candidates array
     {
